ffa6aaef8e9d566877a0c5b0d0c56deeac473460
Sami El-sayed <samielsayed39@gmail.com>
2025-05-22 13:29:38 -0600

    Removed cursor from worldspace. Fixed jitter

--- Assets/Scripts/UI/CursorManager.cs
+++ Assets/Scripts/UI/CursorManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CursorManager : MonoBehaviour
 {
-    public float pixelsPerUnit = 16f; // Match this with your Pixel Perfect Camera
+    
 
     void Start()
     {
@@ -13,16 +13,6 @@ public class CursorManager : MonoBehaviour
 
     void LateUpdate()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0f;
-
-        // Snap to pixel grid
-        Vector3 snappedPos = new Vector3(
-            Mathf.Round(mousePos.x * pixelsPerUnit) / pixelsPerUnit,
-            Mathf.Round(mousePos.y * pixelsPerUnit) / pixelsPerUnit,
-            0f
-        );
-
-        transform.position = snappedPos;
+        transform.position = Input.mousePosition;
     }
 }
