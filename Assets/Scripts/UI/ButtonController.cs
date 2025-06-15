6b4b35c1d7d5daff942891dfeb099cc3bd87621f
Sami El-sayed <samielsayed39@gmail.com>
2025-06-15 14:02:11 -0600

    Start screen button inputs and animations

--- Assets/Scripts/UI/ButtonController.cs
+++ Assets/Scripts/UI/ButtonController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class ButtonController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+
+    public void OnPlayPressed()
     {
-        
+        Debug.Log("Play pressed");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnControlsPressed()
     {
-        
+        Debug.Log("Controls pressed");
+    }
+
+    public void OnExitPressed()
+    {
+        Debug.Log("Exit pressed");
     }
 }
