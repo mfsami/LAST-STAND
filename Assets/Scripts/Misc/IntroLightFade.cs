63c5d2974cf87517c17fe9e8a81e9199986a7f11
Sami El-sayed <samielsayed39@gmail.com>
2025-06-15 22:20:38 -0600

    Started light fade

---
+++ Assets/Scripts/Misc/IntroLightFade.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class IntroLightFade : MonoBehaviour
+{
+
+    public Light2D globalLight;
+    public Light2D campfireLight;
+    public Light2D playerLight;
+    public Light2D cigaretteLight;
+
+    public float fadeDuration = 2f;
+
+    public GameObject enemySpawner;
+
+    private void Start()
+    {
+        // Start all lights at 0
+        globalLight.intensity = 0;
+        campfireLight.intensity = 0;
+        playerLight.intensity = 0;
+        cigaretteLight.intensity = 0;
+
+        // Begin the light sequence
+        StartCoroutine(FadeInSequence());
+    }
+
+    IEnumerator FadeInSequence()
+    {
+        // Step 1: Campfire warms up
+        yield return StartCoroutine(FadeLight(campfireLight, 0, 2.33f, 5f)); // slow burn
+        yield return new WaitForSeconds(0.5f);
+
+        // Step 2: Global + player + cigarette light
+        StartCoroutine(FadeLight(globalLight, 0, 0.14f, fadeDuration));
+        StartCoroutine(FadeLight(playerLight, 0, 0.54f, fadeDuration));
+        StartCoroutine(FadeLight(cigaretteLight, 0, 7.57f, fadeDuration));
+
+        yield return new WaitForSeconds(fadeDuration + 0.5f);
+
+        // Start spawning enemies
+        enemySpawner.SetActive(true);
+        Debug.Log("Game starts");
+    }
+
+    IEnumerator FadeLight(Light2D light, float from, float to, float duration)
+    {
+        float elapsed = 0;
+        light.intensity = from;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            light.intensity = Mathf.Lerp(from, to, elapsed / duration);
+            yield return null;
+        }
+
+        light.intensity = to;
+    }
+}
