1fec82c7e38d5b93c50e709b1d6d038f8a48972d
Sami El-sayed <samielsayed39@gmail.com>
2025-06-16 14:59:03 -0600

    Death screen sounds

--- Assets/Scripts/UI/DeathScreenController.cs
+++ Assets/Scripts/UI/DeathScreenController.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class DeathScreenController : MonoBehaviour
 {
-    public GameObject deathScreen; 
-    
+    public GameObject deathScreen;
+
+    public AudioSource bellSource;
+    public AudioSource choirSource;
+    public AudioSource ambientSource;
+
+    public AudioClip bell;
+    public AudioClip choir;
+    public AudioClip ambient;
+
     public void ShowYouDied()
     {
         deathScreen.SetActive(true);
+
+        bellSource.clip = bell;
+        bellSource.Play();
+
+        choirSource.clip = choir;
+        choirSource.Play();
+
+        ambientSource.clip = ambient;
+        ambientSource.Play();
     }
 }
