8f1baed07327560a9c9f231e315f4270e9f9b8e2
Sami El-sayed <samielsayed39@gmail.com>
2025-06-17 13:13:49 -0600

    Fixed coin value bug

--- Assets/Scripts/Data/CoinPickup.cs
+++ Assets/Scripts/Data/CoinPickup.cs
@@ -4,26 +4,25 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
-    public int coinValue = 1; // Set value in the Inspector
+    public int coinValue = 1;
 
-    
+    private bool pickedUp = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (pickedUp) return; // Prevent duplicate triggers
         if (collision.CompareTag("Player"))
         {
-            PlayerData playerData = collision.GetComponent<PlayerData>();
+            pickedUp = true;
 
-            // Does the object i just collided with have a game data componenet
+            PlayerData playerData = collision.GetComponent<PlayerData>();
             if (playerData != null)
             {
                 playerData.AddCoin(coinValue);
                 playerData.totalCoinsCollected += coinValue;
-
             }
 
             Destroy(gameObject);
         }
     }
 }
-
